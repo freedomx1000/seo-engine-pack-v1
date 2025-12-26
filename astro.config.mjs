5e1176880444304404ee8d9d3ff94cb3c68ca644
freedomx1000 <freedom.sistemas@gmail.com>
2025-12-27 00:06:44 +0100

    fix: remove astrojs sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://seo-engine-pack-v1-q5wd.vercel.app",
-  integrations: [sitemap()],
 });
 
