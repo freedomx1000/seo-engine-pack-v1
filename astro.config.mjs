11d863d592ca99a21a0bd7fb07a13b5a5f51ff66
freedomx1000 <freedom.sistemas@gmail.com>
2025-12-27 05:53:50 +0100

    Add Vercel adapter and hybrid output configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
+  output: "hybrid",
+  adapter: vercel(),
   site: "https://seo-engine-pack-v1-q5wd.vercel.app",
 });
-
